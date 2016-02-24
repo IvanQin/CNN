e9aec008e290ae578c06ff8608ce4d54a59df1f0
Qinyifan <yifanq@kth.se>
2016-02-24 15:10:30 +0800

    change the dataset to MNIST

--- CNN/test_example_CNN.m
+++ CNN/test_example_CNN.m
@@ -1,13 +1,20 @@
-global ti total;
+global ti total zero;
 ti=0;
 total=0;
+zero=0;
 %function test_example_CNN
 load mnist_uint8;
+%load ('../cifar-10-batches-mat/CIFAR10_uint8.mat');
 addpath('./util/');
 train_x = double(reshape(train_x',28,28,60000))/255;
 test_x = double(reshape(test_x',28,28,10000))/255;
 train_y = double(train_y');
 test_y = double(test_y');
+
+%train_x = double(reshape(train_x',32,32,50000))/255;
+%test_x = double(reshape(test_x',32,32,10000))/255;
+%train_y = double(train_y');
+%test_y = double(test_y');
 
 
 %% ex1 Train a 6c-2s-12c-2s Convolutional neural network 
@@ -18,10 +25,10 @@ rand('state',0)
 
 cnn.layers = {
     struct('type', 'i') %input layer
-    struct('type', 'c', 'outputmaps', 6, 'kernelsize', 5) %convolution layer
+    struct('type', 'c', 'outputmaps', 6, 'kernelsize', 5) %convolution layer 6 5
     struct('type', 's', 'scale', 2) %sub sampling layer
-    struct('type', 'c', 'outputmaps', 12, 'kernelsize', 5) %convolution layer
-    struct('type', 's', 'scale', 2) %subsampling layer
+    struct('type', 'c', 'outputmaps', 12, 'kernelsize', 5) %convolution layer 6 5
+    struct('type', 's', 'scale', 2) %sub sampling layer
 };
 
 
@@ -38,9 +45,7 @@ cnn = cnntrain(cnn, train_x, train_y, opts);
 figure; plot(cnn.rL);grid on;
 assert(er<0.12, 'Too big error');
 disp(er);
-disp(bad);
-disp('ti=');
-disp(ti);
-disp('total=');
-disp(total);
+disp(['ti= ' num2str(ti)]);
+disp(['total= ' num2str(total)]);
+disp(['zero= ' num2str(zero)]);
 clear global;
